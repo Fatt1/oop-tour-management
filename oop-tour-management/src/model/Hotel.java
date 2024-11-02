b0246fb69ff7f2d57f1aad320c17edd9cfe89d7c
LAPTOP-4515FHIG\HP <aovgamer000@gmail.com>
2024-11-02 11:54:14 +0700

    add serveral code  lines into hotel

--- oop-tour-management/src/model/Hotel.java
+++ oop-tour-management/src/model/Hotel.java
@@ -1,13 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author User
- */
+
 public class Hotel {
+
+    private String hotelID;
+    private String hotelName;
+    private String phoneNumber;
+    private String address;
+
+    public Hotel(String hotelID, String hotelName, String phoneNumber, String address) {
+        this.hotelID = hotelID;
+        this.hotelName = hotelName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+
+    }
+
+    public Hotel() {
+
+    }
+
+    public String getHotelID() {
+        return hotelID;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setHotelID(String hotelID) {
+        this.hotelID = hotelID;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" + "hotelID=" + hotelID + ", hotelName=" + hotelName + ", phoneNumber=" + phoneNumber + ", address=" + address + '}';
+    }
+
+    public void display() {
+        System.out.printf("|%-6s|%-25s|%-14s|%-25s|\n", hotelID, hotelName, phoneNumber, address);
+    }
     
 }
--- oop-tour-management/src/oop/tour/management/OopTourManagement.java
+++ oop-tour-management/src/oop/tour/management/OopTourManagement.java
@@ -1,22 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package oop.tour.management;
 
+import model.Hotel;
 import util.MyUtil;
 
-/**
- *
- * @author User
- */
+
 public class OopTourManagement {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        
+        Hotel H1 = new Hotel("H001", "The Sun", "901371283", "Ho Chi Minh City");
+        H1.display();
     }
     
 }
