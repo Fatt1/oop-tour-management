e7a18e7165f6ccf6112d69b1749ba69b2570c998
Fatt1 <phat3022005@gmail.com>
2024-11-02 19:00:21 +0700

    code remove funct in VehicleList

--- oop-tour-management/src/lists/VehicleList.java
+++ oop-tour-management/src/lists/VehicleList.java
@@ -10,6 +10,7 @@ import interfaces.IManager;
 import interfaces.LoadData;
 import interfaces.SaveData;
 import java.util.Arrays;
+import java.util.Scanner;
 import model.Vehicle;
 import util.MyUtil;
 
@@ -17,10 +18,13 @@ import util.MyUtil;
  *
  * @author User
  */
-public class VehicleList implements IManager<Vehicle>{
+public class VehicleList implements IManager<Vehicle> {
+
     private Vehicle[] vehicleList;
     private int existedVehicle;
     private String header = String.format("|%-6s|%-15s|%-15s|%-14s|%-6s|", "ID", "NAME", "COMPANY", "PHONE", "SEATS");
+    private Scanner sc = new Scanner(System.in);
+
     public VehicleList() {
         vehicleList = new Vehicle[0];
         existedVehicle = 0;
@@ -30,11 +34,11 @@ public class VehicleList implements IManager<Vehicle>{
     public void add() {
         String id;
         int duplicateId;
-        do {            
-            id  = MyUtil.getId("Input vehicle id (VXXX): ", "The format of Id is (VXXX) "
-                + "X stands for digit(0-9)", "^[v|V]\\d{3}$");
+        do {
+            id = MyUtil.getId("Input vehicle id (VXXX): ", "The format of Id is (VXXX) "
+                    + "X stands for digit(0-9)", "^[v|V]\\d{3}$");
             duplicateId = searchById(id);
-            if(duplicateId >= 0){
+            if (duplicateId >= 0) {
                 System.out.println("ID is duplicate. Please input another id!");
             }
         } while (duplicateId >= 0);
@@ -42,10 +46,10 @@ public class VehicleList implements IManager<Vehicle>{
         String company = MyUtil.getString("Input company vehicle: ", "The company is required");
         String phoneNumber = MyUtil.getString("Input phone vehicle: ", "The phone is required");
         int numberOfSeats = MyUtil.getAnInteger("Input number of seat (1 -> 100): ", "Number of seat is from 1...100", 1, 100);
-        vehicleList = Arrays.copyOf(vehicleList, vehicleList.length + 1);
-        vehicleList[existedVehicle++] = new Vehicle(id, name, company, phoneNumber, numberOfSeats);
+        vehicleList = Arrays.copyOf(vehicleList, vehicleList.length + 1); // cấp thêm vùng nhớ cho vehicleList
+        vehicleList[existedVehicle++] = new Vehicle(id, name, company, phoneNumber, numberOfSeats); // sau khi thêm thì tăng biến đêm lên 
         System.out.println("Vehicle has been added successully");
-        
+
     }
 
     @Override
@@ -55,18 +59,45 @@ public class VehicleList implements IManager<Vehicle>{
 
     @Override
     public void remove() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String id = MyUtil.getString("Input vehicle id: ", "The vehicle id is required");
+        int pos = searchById(id);
+        if (pos < 0) {
+            System.out.println("Not found!!");
+            return;
+        }
+        String choice;
+        do {
+            System.out.print("Choose Y/N: ");
+            choice = sc.nextLine().toLowerCase().trim();
+            if (choice.equalsIgnoreCase("Y")) {
+                for (int i = pos; i < existedVehicle - 1; i++) 
+                    vehicleList[i] = vehicleList[i + 1]; // dịch chuyển những con trỏ từ vị trí pos sang bên trái cho đến khi hết mảng
+                
+                vehicleList = Arrays.copyOf(vehicleList, vehicleList.length - 1); // sau khi di chuyển thì phần tử cuối cùng vẫn tồn tại chưa biến mất hoàn toàn
+                // cần phải cấp phát lại, giảm 1 ô nhớ để vừa đủ danh sách
+                existedVehicle--;
+                System.out.println("Vehicle is removed successfully");
+                return;
+            }
+            else if(choice.equalsIgnoreCase("N"))
+                return;
+            else{
+                System.out.println("Please choice Y/N");
+            }
+
+        } while (true);
+
     }
 
     @Override
     public void printListAscendingById() {
-        if(vehicleList.length == 0){
+        if (vehicleList.length == 0) {
             System.out.println("Empty List!!");
             return;
         }
         System.out.println(header);
         Arrays.sort(vehicleList, (o1, o2) -> o1.getId().compareToIgnoreCase(o2.getId())); // Lambda Expression
-        
+
         for (Vehicle v : vehicleList) {
             v.display();
         }
@@ -75,66 +106,71 @@ public class VehicleList implements IManager<Vehicle>{
     @Override
     public void searchById() {
         String id = MyUtil.getString("Input vehicle id: ", "The vehicle id is required");
-        Vehicle x  = searchObjectById(id);
-        if(x == null){
+        Vehicle x = searchObjectById(id);
+        if (x == null) {
             System.out.println("Not found!!");
             return;
         }
         System.out.println("Here is your vehicle that you're searching");
         System.out.println(header);
         x.display();
-        
+
     }
 
     @Override
     public int searchById(String id) {
-        if(vehicleList.length == 0) 
+        if (vehicleList.length == 0) {
             return -1;
+        }
         for (int i = 0; i < existedVehicle; i++) {
-            if(vehicleList[i].getId().equalsIgnoreCase(id))
+            if (vehicleList[i].getId().equalsIgnoreCase(id)) {
                 return i;
+            }
         }
         return -1;
     }
 
     @Override
     public Vehicle searchObjectById(String id) {
-         if(vehicleList.length == 0) 
+        if (vehicleList.length == 0) {
             return null;
+        }
         for (Vehicle v : vehicleList) {
-            if(v.getId().equalsIgnoreCase(id))
+            if (v.getId().equalsIgnoreCase(id)) {
                 return v;
+            }
         }
         return null;
     }
 
     @Override
     public void ReadData(LoadData loadData) {
-        Object [] obj = loadData.read(); // vì hàm loadData.read trả về mảng Object[] nên phải khái báo 1 mảng object
-                                         // không thể ép kiểu về như vehicleList = (Vehicle[])loadData.read()
-                                         // vì java không cho phép ép 1 mảng tổng quát về 1 mảng cụ thể
+        Object[] obj = loadData.read(); // vì hàm loadData.read trả về mảng Object[] nên phải khái báo 1 mảng object
+        // không thể ép kiểu về như vehicleList = (Vehicle[])loadData.read()
+        // vì java không cho phép ép 1 mảng tổng quát về 1 mảng cụ thể
         for (Object o : obj) {
             vehicleList = Arrays.copyOf(vehicleList, vehicleList.length + 1);
             vehicleList[existedVehicle++] = (Vehicle) o; // chỉ có thể ép kiểu từ 1 Object về 1 object cụ thể
-                                                         // cho nên để giải quyế phải ép kiểu cho từng phần từ 1
+            // cho nên để giải quyế phải ép kiểu cho từng phần từ 1
         }
-         
+
     }
 
     @Override
     public void saveToDate(SaveData saveData) {
         saveData.save(vehicleList);
     }
-    
+
     // test
+    // để chạy hàm test này thì bấm Shift + F6
     public static void main(String[] args) {
         VehicleList v = new VehicleList();
         v.ReadData(new LoadDataFromFile("Files/Vehicles.dat"));
         
         v.printListAscendingById();
-        
-        v.saveToDate(new SaveDataToFile("Files/Vehicles.dat"));
-        
+
+       v.saveToDate(new SaveDataToFile("Files/Vehicles.dat"));
+
     }
-   
+
 }
