f5c47345f2e2c3c46cd02fd1cee43d4dfd846779
LAPTOP-4515FHIG\HP <aovgamer000@gmail.com>
2024-11-05 15:59:22 +0700

    create restaurantList and restaurant

--- oop-tour-management/src/model/Restaurant.java
+++ oop-tour-management/src/model/Restaurant.java
@@ -1,13 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author HP
- */
+
 public class Restaurant {
+    private String restaurantID;
+    private String restaurantName;
+    private String phoneNumber;
+    private String address;
     
+    public Restaurant(String restaurantID, String restaurantName, String phoneNumber, String address){
+        this.restaurantID = restaurantID;
+        this.restaurantName = restaurantName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+    
+    public Restaurant(){
+        
+    }
+    
+    public String getRestaurantID(){
+        return restaurantID;
+    }
+    
+    public String getRestaurantName(){
+        return restaurantName;
+    }
+    
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    
+    public String getaddress(){
+        return address;
+    }
+    
+    public void restaurantID(){
+        this.restaurantID = restaurantID;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" + "restaurantID=" + restaurantID + ", restaurantName=" + restaurantName + ", phoneNumber=" + phoneNumber + ", address=" + address + '}';
+    }
+    
+    public void display() {
+        System.out.printf("|%-6s|%-25s|%-14s|%-25s|\n", restaurantID, restaurantName, phoneNumber, address);
+    }
 }
--- oop-tour-management/src/oop/tour/management/OopTourManagement.java
+++ oop-tour-management/src/oop/tour/management/OopTourManagement.java
@@ -1,31 +1,17 @@
 
 package oop.tour.management;
 
-<<<<<<< HEAD
 import model.Hotel;
 import util.MyUtil;
 import lists.HotelList;
-=======
 import lists.VehicleList;
 
-
-
->>>>>>> 851f637284ab540b88f8a6099ba2d775907c9840
 
 public class OopTourManagement {
 
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        
-        HotelList HLT = new HotelList();
-        HLT.add();
-        HLT.add();
-        HLT.add();
-        HLT.printListAscendingById();
-=======
-       
->>>>>>> 851f637284ab540b88f8a6099ba2d775907c9840
+
     }
     
 }
