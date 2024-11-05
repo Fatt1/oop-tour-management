54b6951812dd1df95a5df3e3d86cc444c04bdd62
LAPTOP-4515FHIG\HP <aovgamer000@gmail.com>
2024-11-05 15:54:58 +0700

    ....

--- oop-tour-management/src/lists/HotelList.java
+++ oop-tour-management/src/lists/HotelList.java
@@ -3,6 +3,8 @@ package lists;
 import interfaces.IManager;
 import interfaces.LoadData;
 import interfaces.SaveData;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import model.Hotel;
 import util.MyUtil;
@@ -11,42 +13,127 @@ public class HotelList implements IManager<Hotel> {
 
     private Hotel[] hotelList;
     private int existedHotel;
+    private Scanner sc = new Scanner(System.in);
 
     public HotelList() {
-        hotelList = new Hotel[200];
+        hotelList = new Hotel[0];
         existedHotel = 0;
     }
-    
-    Scanner sc = new Scanner(System.in);
+
     @Override
 
     public void add() {
+        String id;
+        int duplicatedId;
+        do {
+            id = MyUtil.getId("Input hotel id (HXXX): ", "The format of Id is (HXXX) "
+                    + "X stands for digit(0-9)", "^[h|H]\\d{3}$");
+            duplicatedId = searchById(id);
+            if (duplicatedId >= 0) {
+                System.out.println("ID is duplicated. Please input another id!");
+            }
+        } while (duplicatedId >= 0);
+
         String hotelID = MyUtil.getId("Please input the hotel Id: ", "!This information is crucial!", "^[h|H]\\d{3}");
         String hotelName = MyUtil.getString("Please input the hotel name: ", "!This information is crucial!");
         String phoneNumber = MyUtil.getString("Please input hotel's phone numebr: ", "!This information is crucial!");
         String address = MyUtil.getString("Please input hotel's address: ", "!This information is crucial!");
 
-        hotelList[existedHotel] = new Hotel(hotelID, hotelName, phoneNumber, address);
-        existedHotel++;
+        hotelList = Arrays.copyOf(hotelList, hotelList.length + 1);
+        hotelList[existedHotel++] = new Hotel(hotelID, hotelName, phoneNumber, address);
+        System.out.println("Hotel has been added completely!");
     }
 
     @Override
     public void update() {
-
+        int n;
+        do {
+            System.out.println("#1.Update Hotel Name   ");
+            System.out.println("#2.Update Phone Number ");
+            System.out.println("#3.Update Address      ");
+            System.out.println("#4.Exit update function");
+            n = MyUtil.getAnInteger("Opt from 1 to 3 different properties to update: ", "Please input a numeber!");
+            System.out.println("------------------------------------------------");
+            int point; // point to a particular object in the list
+            switch (n) {
+                case 1:
+                    point = MyUtil.getAnInteger("Enter the position of the object you want to update: ", "The position cant be found");
+                    if (point > existedHotel) {
+                        break;
+                    }
+                    String newHotelName = MyUtil.getString("Enter new Hotel Name: ", "This information is crucial");
+                    hotelList[point].setHotelName(newHotelName);
+                    System.out.println("!-CHANGE SUCCESSFULLY-!");
+                    hotelList[point].display();
+                    System.out.println("            ");
+                    break;
+                case 2:
+                    point = MyUtil.getAnInteger("Enter the position of the object you want to update: ", "The position cant be found");
+                    if (point > existedHotel) {
+                        break;
+                    }
+                    String newPhoneNumber = MyUtil.getString("Enter new Hotel New Phone Number: ", "This information is crucial");
+                    hotelList[point].setPhoneNumber(newPhoneNumber);
+                    System.out.println("!-CHANGE SUCCESSFULLY-!");
+                    hotelList[point].display();
+                    System.out.println("            ");
+                    break;
+                case 3:
+                    point = MyUtil.getAnInteger("Enter the position of the object you want to update: ", "The position cant be found");
+                    if (point > existedHotel) {
+                        break;
+                    }
+                    String newAddress = MyUtil.getString("Enter new Hotel New Address: ", "This information is crucial");
+                    hotelList[point].setAddress(newAddress);
+                    System.out.println("!-CHANGE SUCCESSFULLY-!");
+                    hotelList[point].display();
+                    System.out.println("            ");
+                    break;
+                case 4:
+                    break;
+                default:
+                    throw new AssertionError("The option cant be found!");
+            }
+        } while (n != 4);
     }
 
     @Override
     public void remove() {
+        String id = MyUtil.getString("Enter the ID you want to find: ", "This information is crucial");
+        int position = searchById(id);
+        if (position < 0) {
+            System.out.println("Not Found!");
+            return;
+        }
+        String choice;
+        do {
+            System.out.print("Choose Y/N: ");
+            choice = sc.nextLine().toLowerCase().trim();
+            if (choice.equalsIgnoreCase("Y")) {
+                for (int i = position; i < existedHotel - 1; i++)
+                    hotelList[i] = hotelList[i + 1];
+
+                hotelList = Arrays.copyOf(hotelList, hotelList.length - 1); // update the list length again
+
+                existedHotel--;
+                System.out.println("!-THE HOTEL HAS SUCCESSFULLY REMOVED-!");
+                return;
+            } else if (choice.equalsIgnoreCase("N")){
+                return;
+            }else {
+                System.out.println("Please choose Y/N");
+            }
+        } while (true);
     }
 
     @Override
     public void printListAscendingById() {
         for (int i = 0; i < existedHotel - 1; i++) {
-            for (int j = i+1; j < existedHotel; j++) {
+            for (int j = i + 1; j < existedHotel; j++) {
                 if (hotelList[i].getHotelID().compareTo(hotelList[j].getHotelID()) > 0) {
-                     Hotel HH1 = hotelList[i];
-                     hotelList[i] =  hotelList[j];
-                     hotelList[j] = HH1;
+                    Hotel HH1 = hotelList[i];
+                    hotelList[i] = hotelList[j];
+                    hotelList[j] = HH1;
                 }
             }
         }
@@ -57,25 +144,39 @@ public class HotelList implements IManager<Hotel> {
 
     @Override
     public void searchById() {
-        System.out.print("Enter the id: ");
-        String newID; 
-        
+        String newID = MyUtil.getString("Enter the ID: ", "The ID is required");
         for (int i = 0; i < existedHotel; i++) {
-            if(hotelList[i].getHotelID().equalsIgnoreCase(newID) == )
+            if (hotelList[i].getHotelID().equalsIgnoreCase(newID) == true) {
+                hotelList[i].display();
+            }
         }
     }
 
     @Override
-<<<<<<< HEAD
-    public void searchById(String id) {
-=======
     public int searchById(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
->>>>>>> 851f637284ab540b88f8a6099ba2d775907c9840
+        if (hotelList.length == 0) {
+            return -1;
+        } else {
+            for (int i = 0; i < existedHotel; i++) {
+                if (hotelList[i].getHotelID().equalsIgnoreCase(id)) {
+                    return 1;
+                }
+            }
+            return -1;
+        }
     }
 
     @Override
     public Hotel searchObjectById(String id) {
+        if(hotelList.length == 0){
+            System.out.println("Not Found!");
+            return null;
+        }
+        for (Hotel x : hotelList) {
+            if(x.getHotelID().equalsIgnoreCase(id)){
+                return x;
+            }
+        }
         return null;
     }
 
